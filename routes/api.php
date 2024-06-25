629982be879e71641698677ccb71e458dc6a04ba
Erik Langhaug <erik.langhaug@h5p.group>
2024-06-25 12:19:49 +0200

    Episode 4 complete
    Slight workaround for api/v1 prefix

--- routes/api.php
+++ routes/api.php
@@ -8,8 +8,8 @@ use Illuminate\Support\Facades\Route;
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 
-Route::get('/tickets', function () {
-    return Ticket::all();
+Route::prefix('v1')->group(function () {
+    require_once __DIR__ . '/api_v1.php';
 });
 
 Route::get('/user', function (Request $request) {
